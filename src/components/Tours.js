2e6c9efe4d64236afc528d6e4a5f3e3fbb844c47
abbos91 <abbos.shomurodov91@gmail.com>
2026-06-17 00:58:08 +0500

    title updated

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -1,13 +1,9 @@
 import React from "react";
-
+import { Title } from "./Title";
 function Tours() {
     return (
         <section className="section" id="tours">
-            <div className="section-title">
-                <h2>
-                    featured <span>tours</span>
-                </h2>
-            </div>
+            <Title title="featured" subtitle="tours" />
 
             <div className="section-center featured-center">
                 <article className="tour-card">
